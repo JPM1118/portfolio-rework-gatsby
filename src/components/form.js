1cf54fb8ef21b4c07d3bdaa202145188ffca7e7a
Jack McCormick <johnmccormick1118@gmail.com>
2019-03-11 10:00:00 -0500

    Finished contact styling.

--- src/components/form.js
+++ src/components/form.js
@@ -3,18 +3,21 @@ import React from 'react';
 import formStyle from './componentStyles/form.module.scss';
 
 const Form = () => (
-  <form method="post" action="#">
+  <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
     <div className={formStyle.name}>
-      <label htmlFor="name">Name</label>
-      <input type="text" name="name" id="name" />
+      <label htmlFor="name">Name
+        <input type="text" name="name" id="name" required />
+      </label>
     </div>
     <div className={formStyle.email}>
-      <label htmlFor="email">Email</label>
-      <input type="text" name="email" id="email" />
+      <label htmlFor="email">Email
+        <input type="email" name="email" id="email" required />
+      </label>
     </div>
     <div className={formStyle.message}>
-      <label htmlFor="message">Message</label>
-      <textarea name="message" id="message" rows="6"></textarea>
+      <label htmlFor="message">Message
+        <textarea name="message" id="message" rows="6" required />
+      </label>
     </div>
     <button type="submit" id="submit" className={formStyle.button}>Submit</button>
   </form>
