9932a1bf9a853fd84baaa19dd802ae195902fbf8
Jack McCormick <johnmccormick1118@gmail.com>
2019-03-11 10:23:03 -0500

    Added id to contact page.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,10 +3,10 @@ import React from 'react'
 import Form from '../components/form';
 import contactStyles from './pageStyles/contact.module.scss';
 
-const ContactPage = () => (
+const ContactPage = (props) => (
   <div className={contactStyles.background}>
     <div className={contactStyles.container}>
-      <h1 className={contactStyles.title}>Contact</h1>
+      <h1 className={contactStyles.title} id={props._id}>Contact</h1>
       <div className={contactStyles.formContainer}>
         <Form />
       </div>
